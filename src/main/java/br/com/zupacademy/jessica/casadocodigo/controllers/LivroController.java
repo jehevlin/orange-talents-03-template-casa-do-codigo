0c6f6c486be520f3e22dbb191a6a23e0f0b94c88
Jessica <jessica.hevlin@gmail.com>
2021-04-27 22:57:45 -0300

    Pagina detalhe livro

--- src/main/java/br/com/zupacademy/jessica/casadocodigo/controllers/LivroController.java
+++ src/main/java/br/com/zupacademy/jessica/casadocodigo/controllers/LivroController.java
@@ -3,7 +3,9 @@ package br.com.zupacademy.jessica.casadocodigo.controllers;
 import br.com.zupacademy.jessica.casadocodigo.models.Livro;
 import br.com.zupacademy.jessica.casadocodigo.repositories.LivroRepository;
 import br.com.zupacademy.jessica.casadocodigo.requests.CadastrarLivroRequest;
+import br.com.zupacademy.jessica.casadocodigo.responses.DetalheLivroResponse;
 import br.com.zupacademy.jessica.casadocodigo.responses.LivroResponse;
+import org.hibernate.annotations.NotFound;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path ="livros" )
@@ -43,4 +46,16 @@ public class LivroController {
         return new ResponseEntity<>(livros, HttpStatus.OK);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> detalhe(@PathVariable("id") Long id){
+        Optional<Livro> optional = repository.findById(id);
+        if (optional.isPresent()){
+            Livro livro = optional.get();
+            return new ResponseEntity<>(new DetalheLivroResponse(livro.getId(), livro.getTitulo(),
+                    livro.getResumo(), livro.getAutor().getNome(), livro.getAutor().getDescricao(),
+                    livro.getPreco(), livro.getSumario(), livro.getPaginas(), livro.getIsbn()), HttpStatus.OK);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
